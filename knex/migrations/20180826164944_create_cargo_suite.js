57abf8836a46b5e980ca32d049c5e4d1c31b7cd0
xsong <xugang.song@advance.ai>
2018-09-04 22:04:30 +0800

    Update: format js files

--- knex/migrations/20180826164944_create_cargo_suite.js
+++ knex/migrations/20180826164944_create_cargo_suite.js
@@ -1,11 +1,11 @@
-exports.up = function (knex) {
-  return knex.schema.createTable('cargo_suite', function (t) {
+exports.up = function(knex) {
+  return knex.schema.createTable('cargo_suite', function(t) {
     t.increments('id').primary()
     t.string('suite_no').notNullable()
     t.string('suite_name').notNullable()
     t.string('program_belong')
   })
 }
-exports.down = function (knex) {
+exports.down = function(knex) {
   return knex.schema.dropTableIfExists('cargo_suite')
 }
