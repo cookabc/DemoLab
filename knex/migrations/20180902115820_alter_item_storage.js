5f087aa0f66eb50e86af7c3fa15f8ad27126902e
chuangcius <chuangcius@gmail.com>
2018-09-16 15:49:41 +0800

    Update: udpate tebles

--- knex/migrations/20180902115820_alter_item_storage.js
+++ knex/migrations/20180902115820_alter_item_storage.js
@@ -1,14 +1,14 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table('item_storage', function(table) {
-      table.string('expired_date').alter()
+    knex.schema.alterTable('item_storage', function(table) {
+      table.string('expired_date')
     }),
   ])
 }
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table('item_storage', function(table) {
+    knex.schema.alterTable('item_storage', function(table) {
       table.dropColumn('expired_date')
     }),
   ])
--- knex/migrations/20180905204848_alter_item.js
+++ knex/migrations/20180905204848_alter_item.js
@@ -3,6 +3,12 @@ exports.up = function(knex, Promise) {
     knex.schema.alterTable('cargo_item', function(table) {
       table.unique(['item_no', 'item_name', 'program_belong', 'store_temperature'], 'uniqueIndex')
     }),
+    knex.schema.table('cargo_item', function(table) {
+      table.string('item_no').alter()
+    }),
+    knex.schema.alterTable('cargo_item', function(table) {
+      table.integer('safe_number')
+    }),
   ])
 }
 
@@ -10,6 +16,12 @@ exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.alterTable('cargo_item', function(table) {
       table.dropColumn(['item_no', 'item_name', 'program_belong', 'store_temperature'])
+    }),
+    knex.schema.table('cargo_item', function(table) {
+      table.dropColumn(['item_no'])
+    }),
+    knex.schema.alterTable('cargo_item', function(table) {
+      table.dropColumn(['safe_number'])
     }),
   ])
 }
