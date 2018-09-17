17886433f5dc9ca9059dbc0b9e22faf0c42a7568
chuangcius <chuangcius@gmail.com>
2018-09-17 10:08:49 +0800

    Revert "Update: trim api fields"
    This reverts commit cd00db2122e17475cc69569ac0f6e940677a3b19.

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.use(bodyParser.json())
 app.post('/api/createItem', (req, res) => {
   storeItem
     .createItem({
-      item_no: req.body.itemNo.trim(),
-      item_name: req.body.itemName.trim(),
-      program_belong: req.body.programBelong.trim(),
-      store_temperature: req.body.storeTemperature.trim(),
+      item_no: req.body.itemNo,
+      item_name: req.body.itemName,
+      program_belong: req.body.programBelong,
+      store_temperature: req.body.storeTemperature,
     })
     .then(data => {
       console.log(data)
@@ -28,11 +28,11 @@ app.post('/api/createItem', (req, res) => {
 app.get('/api/getItem', (req, res) => {
   storeItem
     .getItem({
-      id: req.query.id.trim(),
-      item_no: req.query.itemNo.trim(),
-      item_name: req.query.itemName.trim(),
-      program_belong: req.query.programBelong.trim(),
-      store_temperature: req.query.storeTemperature.trim(),
+      id: req.query.id,
+      item_no: req.query.itemNo,
+      item_name: req.query.itemName,
+      program_belong: req.query.programBelong,
+      store_temperature: req.query.storeTemperature,
     })
     .then(data => {
       if (data) {
@@ -56,11 +56,11 @@ app.get('/api/getItemAndStorage', (req, res) => {
 app.post('/api/updateItem', (req, res) => {
   storeItem
     .updateItem({
-      id: req.body.id.trim(),
-      item_no: req.body.itemNo.trim(),
-      item_name: req.body.itemName.trim(),
-      program_belong: req.body.programBelong.trim(),
-      store_temperature: req.body.storeTemperature.trim(),
+      id: req.body.id,
+      item_no: req.body.itemNo,
+      item_name: req.body.itemName,
+      program_belong: req.body.programBelong,
+      store_temperature: req.body.storeTemperature,
     })
     .then(data => {
       if (data.code) {
@@ -71,7 +71,7 @@ app.post('/api/updateItem', (req, res) => {
     })
 })
 app.post('/api/deleteItem', (req, res) => {
-  storeItem.deleteItem({ id: req.body.id.trim() }).then(data => {
+  storeItem.deleteItem({ id: req.body.id }).then(data => {
     if (data.code) {
       res.status(400).json({ code: data.code, data: null, message: 'Bad Request' })
     } else {
@@ -84,10 +84,10 @@ app.post('/api/deleteItem', (req, res) => {
 app.post('/api/createItemStorage', (req, res) => {
   storeItem
     .createItemStorage({
-      item_id: req.body.itemId.trim(),
-      store_position: req.body.storePosition.trim(),
-      store_number: req.body.storeNumber.trim(),
-      expired_date: req.body.expiredDate.trim(),
+      item_id: req.body.itemId,
+      store_position: req.body.storePosition,
+      store_number: req.body.storeNumber,
+      expired_date: req.body.expiredDate,
       note: req.body.note,
     })
     .then(data => {
@@ -99,7 +99,7 @@ app.post('/api/createItemStorage', (req, res) => {
     })
 })
 app.get('/api/getItemStorage', (req, res) => {
-  storeItem.getItemStorage({ item_id: req.query.itemId.trim() }).then(data => {
+  storeItem.getItemStorage({ item_id: req.query.itemId }).then(data => {
     if (data) {
       res.setHeader('Content-Type', 'application/json')
       res.send(JSON.stringify({ data }))
@@ -111,11 +111,11 @@ app.get('/api/getItemStorage', (req, res) => {
 app.post('/api/updateItemStorage', (req, res) => {
   storeItem
     .updateItemStorage({
-      id: req.body.id.trim(),
-      store_position: req.body.storePosition.trim(),
-      store_number: req.body.storeNumber.trim(),
-      expired_date: req.body.expiredDate.trim(),
-      note: req.body.note.trim(),
+      id: req.body.id,
+      store_position: req.body.storePosition,
+      store_number: req.body.storeNumber,
+      expired_date: req.body.expiredDate,
+      note: req.body.note,
     })
     .then(data => {
       if (data.code) {
@@ -126,7 +126,7 @@ app.post('/api/updateItemStorage', (req, res) => {
     })
 })
 app.post('/api/deleteItemStorage', (req, res) => {
-  storeItem.deleteItemStorage({ id: req.body.id.trim() }).then(data => {
+  storeItem.deleteItemStorage({ id: req.body.id }).then(data => {
     if (data.code) {
       res.status(400).json({ code: data.code, data: null, message: 'Bad Request' })
     } else {
@@ -137,8 +137,8 @@ app.post('/api/deleteItemStorage', (req, res) => {
 app.post('/api/shipItem', (req, res) => {
   storeItem
     .shipItem({
-      id: req.body.id.trim(),
-      ship_number: req.body.shipNumber.trim(),
+      id: req.body.id,
+      ship_number: req.body.shipNumber,
     })
     .then(() => {
       if (data.code) {
@@ -146,14 +146,14 @@ app.post('/api/shipItem', (req, res) => {
       } else {
         shipRecord
           .createRecord({
-            item_storage_id: req.body.id.trim(),
-            item_id: req.body.itemId.trim(),
-            initiator_name: req.body.initiatorName.trim(),
-            initiator_pos_dep: req.body.initiatorPosDep.trim(),
-            initiator_usage: req.body.initiatorUsage.trim(),
-            ship_number: req.body.shipNumber.trim(),
+            item_storage_id: req.body.id,
+            item_id: req.body.itemId,
+            initiator_name: req.body.initiatorName,
+            initiator_pos_dep: req.body.initiatorPosDep,
+            initiator_usage: req.body.initiatorUsage,
+            ship_number: req.body.shipNumber,
             create_date: new Date().getTime(),
-            note: req.body.note.trim(),
+            note: req.body.note,
           })
           .then(data => {
             if (data.code) {
@@ -180,8 +180,8 @@ app.get('/api/getShipRecord', (req, res) => {
 app.post('/api/updateShipRecord', (req, res) => {
   shipRecord
     .updateRecord({
-      id: req.body.id.trim(),
-      note: req.body.note.trim(),
+      id: req.body.id,
+      note: req.body.note,
     })
     .then(data => {
       if (data.code) {
@@ -192,7 +192,7 @@ app.post('/api/updateShipRecord', (req, res) => {
     })
 })
 app.post('/api/deleteShipRecord', (req, res) => {
-  shipRecord.deleteRecord({ id: req.body.id.trim() }).then(data => {
+  shipRecord.deleteRecord({ id: req.body.id }).then(data => {
     if (data.code) {
       res.status(400).json({ code: data.code, data: null, message: 'Bad Request' })
     } else {
@@ -205,9 +205,9 @@ app.post('/api/deleteShipRecord', (req, res) => {
 app.post('/api/createSuite', (req, res) => {
   storeSuite
     .createSuite({
-      suite_no: req.body.suiteNo.trim(),
-      suite_name: req.body.suiteName.trim(),
-      program_belong: req.body.programBelong.trim(),
+      suite_no: req.body.suiteNo,
+      suite_name: req.body.suiteName,
+      program_belong: req.body.programBelong,
     })
     .then(data => {
       if (data.code) {
@@ -220,10 +220,10 @@ app.post('/api/createSuite', (req, res) => {
 app.get('/api/getSuite', (req, res) => {
   storeSuite
     .getSuite({
-      id: req.query.id.trim(),
-      suite_no: req.query.suiteNo.trim(),
-      suite_name: req.query.suiteName.trim(),
-      program_belong: req.query.programBelong.trim(),
+      id: req.query.id,
+      suite_no: req.query.suiteNo,
+      suite_name: req.query.suiteName,
+      program_belong: req.query.programBelong,
     })
     .then(data => {
       if (data) {
@@ -238,10 +238,10 @@ app.post('/api/updateSuite', (req, res) => {
   console.log(req.body)
   storeSuite
     .updateSuite({
-      id: req.body.id.trim(),
-      suite_no: req.body.suiteNo.trim(),
-      suite_name: req.body.suiteName.trim(),
-      program_belong: req.body.programBelong.trim(),
+      id: req.body.id,
+      suite_no: req.body.suiteNo,
+      suite_name: req.body.suiteName,
+      program_belong: req.body.programBelong,
     })
     .then(data => {
       if (data.code) {
@@ -252,7 +252,7 @@ app.post('/api/updateSuite', (req, res) => {
     })
 })
 app.post('/api/deleteSuite', (req, res) => {
-  storeSuite.deleteSuite({ id: req.body.id.trim() }).then(data => {
+  storeSuite.deleteSuite({ id: req.body.id }).then(data => {
     if (data.code) {
       res.status(400).json({ code: data.code, data: null, message: 'Bad Request' })
     } else {
@@ -265,9 +265,9 @@ app.post('/api/deleteSuite', (req, res) => {
 app.post('/api/createSuiteComponent', (req, res) => {
   storeSuite
     .createSuiteComponent({
-      suite_id: req.body.suiteId.trim(),
-      item_id: req.body.itemId.trim(),
-      item_number: req.body.itemNumber.trim(),
+      suite_id: req.body.suiteId,
+      item_id: req.body.itemId,
+      item_number: req.body.itemNumber,
     })
     .then(data => {
       if (data.code) {
@@ -278,7 +278,7 @@ app.post('/api/createSuiteComponent', (req, res) => {
     })
 })
 app.get('/api/getSuiteComponent', (req, res) => {
-  storeSuite.getSuiteComponent({ suite_id: req.query.suiteId.trim() }).then(data => {
+  storeSuite.getSuiteComponent({ suite_id: req.query.suiteId }).then(data => {
     if (data) {
       res.setHeader('Content-Type', 'application/json')
       res.send(JSON.stringify({ data }))
@@ -288,7 +288,7 @@ app.get('/api/getSuiteComponent', (req, res) => {
   })
 })
 app.get('/api/getSuiteComponentItemCount', (req, res) => {
-  storeSuite.getSuiteComponentItemCount({ suite_id: req.query.suiteId.trim() }).then(data => {
+  storeSuite.getSuiteComponentItemCount({ suite_id: req.query.suiteId }).then(data => {
     if (data) {
       res.setHeader('Content-Type', 'application/json')
       res.send(JSON.stringify({ data }))
@@ -300,8 +300,8 @@ app.get('/api/getSuiteComponentItemCount', (req, res) => {
 app.post('/api/deleteSuiteComponent', (req, res) => {
   storeSuite
     .deleteSuiteComponent({
-      suite_id: req.body.suite_id.trim(),
-      item_id: req.body.item_id.trim(),
+      suite_id: req.body.suite_id,
+      item_id: req.body.item_id,
     })
     .then(data => {
       if (data.code) {
