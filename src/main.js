112d820f4054fc1d1d65891620fdaaa185544554
chuangcius <chuangcius@gmail.com>
2018-09-28 21:50:39 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import store from './store'
 import router from './router'
 import http from './plugins/axios'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import './assets/element-variables.scss'
 
 Vue.config.productionTip = false
