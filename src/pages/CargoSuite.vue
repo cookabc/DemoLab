876ef9e1c5632b81ed1e456928f369b5576fe7a0
xsong <xugang.song@advance.ai>
2018-09-28 21:29:31 +0800

    fix bug

--- src/pages/CargoSuite.vue
+++ src/pages/CargoSuite.vue
@@ -76,6 +76,7 @@ export default {
   },
   methods: {
     dateFormatter(timestamp) {
+      if (!timestamp) { return null }
       const date = new Date(parseInt(timestamp))
       return dateFormat(date, 'yyyy-mm-dd')
     },
