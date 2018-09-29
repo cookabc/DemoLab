d8d43a765f8e379b4ae4a96139ef2fe938c639f1
chuangcius <chuangcius@gmail.com>
2018-09-29 18:04:06 +0800

    Update: migration files

--- knex/migrations/20180826164944_create_cargo_suite.js
+++ knex/migrations/20180826164944_create_cargo_suite.js
@@ -1,9 +1,11 @@
 exports.up = function(knex) {
   return knex.schema.createTable('cargo_suite', function(t) {
     t.increments('id').primary()
-    t.string('suite_no').notNullable()
+    t.string('suite_no')
     t.string('suite_name').notNullable()
     t.string('program_belong')
+    t.integer('safe_number')
+    t.unique(['suite_no', 'suite_name', 'program_belong'], 'uniqueIndex')
   })
 }
 exports.down = function(knex) {
