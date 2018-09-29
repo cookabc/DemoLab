eed650f4107aaca644763a5d423f89292a63186d
xsong <xugang.song@advance.ai>
2018-09-29 18:04:06 +0800

    Update: migration files

--- knex/migrations/20180826164920_create_cargo_item.js
+++ knex/migrations/20180826164920_create_cargo_item.js
@@ -1,10 +1,12 @@
 exports.up = function(knex) {
   return knex.schema.createTable('cargo_item', function(t) {
     t.increments('id').primary()
-    t.string('item_no').notNullable()
+    t.string('item_no')
     t.string('item_name').notNullable()
     t.string('program_belong')
-    t.string('store_temperature').notNullable()
+    t.string('store_temperature')
+    t.integer('safe_number')
+    t.unique(['item_no', 'item_name', 'program_belong', 'store_temperature', 'safe_number'], 'uniqueIndex')
   })
 }
 exports.down = function(knex) {
